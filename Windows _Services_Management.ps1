a67d8809ac8c7aa303ab8ba1b8c0192bda22dba4
LokilloBoss <lokillo_aguayo@hotmail.com>
2023-04-10 20:52:22 -0600

    startup-states-windows
    Agregar validación del archivo de entrada: Sería útil agregar validación para asegurarnos de que el archivo de entrada existe y que es un archivo de texto plano.
    
    Utilizar parámetros de PowerShell en lugar de cadenas: Para una mayor legibilidad y claridad, podríamos utilizar los parámetros de PowerShell en lugar de cadenas para las acciones que queremos realizar (Start y Stop). Esto nos permitiría utilizar autocompletado y validación de parámetros en PowerShell para ayudar a prevenir errores tipográficos.
    
    Proporcionar retroalimentación detallada: Actualmente, el script proporciona mensajes de salida simples que indican si se inició o detuvo cada servicio. Sería útil proporcionar información adicional, como el estado actual del servicio antes de tomar una acción y cualquier error que ocurra.
    
    Agregar soporte para múltiples computadoras: Si se desea utilizar el script para manejar los servicios en varias computadoras, se podría agregar un parámetro de entrada adicional para especificar la(s) computadora(s) a las que se debe aplicar el script.

---
+++ Windows _Services_Management.ps1
@@ -0,0 +1,186 @@
+# Lista de servicios a verificar
+$services = @("servicio1", "servicio2", "servicio3")
+
+# Verificar el estado actual de los servicios y tomar la acción deseada
+foreach ($service in $services) {
+    $status = Get-Service -Name $service -ErrorAction SilentlyContinue
+    if ($status.Status -eq "Running") {
+        Write-Host "$service ya se encuentra en ejecución"
+    }
+    elseif ($status.Status -eq "Stopped") {
+        Write-Host "Iniciando $service ..."
+        Start-Service -Name $service
+        Write-Host "$service se ha iniciado correctamente"
+    }
+    else {
+        Write-Host "No se pudo verificar el estado de $service"
+    }
+}
+
+# Verificar el estado actual de los servicios y tomar la acción deseada
+foreach ($service in $services) {
+    $status = Get-Service -Name $service -ErrorAction SilentlyContinue
+    if ($status.Status -eq "Running") {
+        Write-Host "Deteniendo $service ..."
+        Stop-Service -Name $service
+        Write-Host "$service se ha detenido correctamente"
+    }
+    elseif ($status.Status -eq "Stopped") {
+        Write-Host "$service ya se encuentra detenido"
+    }
+    else {
+        Write-Host "No se pudo verificar el estado de $service"
+    }
+}
+
+===
+# Iniciar los servicios que no estén en ejecución
+foreach ($service in $services) {
+    $status = Get-Service -Name $service -ErrorAction SilentlyContinue
+    if ($status.Status -eq "Running") {
+        Write-Host "$service ya se encuentra en ejecución"
+    }
+    elseif ($status.Status -eq "Stopped") {
+        Write-Host "Iniciando $service ..."
+        Start-Service -Name $service
+        Write-Host "$service se ha iniciado correctamente"
+    }
+    else {
+        Write-Host "No se pudo verificar el estado de $service"
+    }
+}
+
+===
+
+# List of services to check
+$services = @("service1", "service2", "service3")
+
+# Check current status of services and take desired action
+foreach ($service in $services) {
+    $status = Get-Service -Name $service -ErrorAction SilentlyContinue
+    if ($status.Status -eq "Running") {
+        Write-Host "Stopping $service ..."
+        Stop-Service -Name $service
+        Write-Host "$service has stopped successfully"
+    }
+    elseif ($status.Status -eq "Stopped") {
+        Write-Host "$service is already stopped"
+    }
+    else {
+        Write-Host "Could not verify status of $service"
+    }
+}
+
+# Check current status of services and take desired action
+foreach ($service in $services) {
+    $status = Get-Service -Name $service -ErrorAction SilentlyContinue
+    if ($status.Status -eq "Running") {
+        Write-Host "$service is already running"
+    }
+    elseif ($status.Status -eq "Stopped") {
+        Write-Host "Starting $service ..."
+        Start-Service -Name $service
+        Write-Host "$service has started successfully"
+    }
+    else {
+        Write-Host "Could not verify status of $service"
+    }
+}
+
+
+===
+
+# Parámetro para indicar si se deben iniciar o detener los servicios
+param(
+    [Parameter(Mandatory=$true,Position=0)]
+    [ValidateSet('Start','Stop')]
+    [string]$Action,
+    [Parameter(Mandatory=$true,Position=1)]
+    [string]$FilePath
+)
+
+# Obtener la lista de servicios desde el archivo especificado
+$services = Get-Content $FilePath
+
+# List of services to check
+$services = @("service1", "service2", "service3")
+
+# Validar y tomar acción sobre cada servicio de la lista
+foreach ($service in $services) {
+    $status = Get-Service -Name $service -ErrorAction SilentlyContinue
+    if ($status) {
+        if ($status.Status -eq "Running" -and $Action -eq "Start") {
+            Write-Host "$service ya se encuentra en ejecución"
+        }
+        elseif ($status.Status -eq "Stopped" -and $Action -eq "Stop") {
+            Write-Host "$service ya se encuentra detenido"
+        }
+        else {
+            Write-Host "$Action-ndo $service ..."
+            if ($Action -eq "Start") {
+                Start-Service -Name $service
+            }
+            else {
+                Stop-Service -Name $service
+            }
+            Write-Host "$service se ha $Action correctamente"
+        }
+    }
+    else {
+        Write-Host "No se pudo verificar el estado de $service"
+    }
+}
+
+
+======
+
+param(
+    [Parameter(Mandatory=$true, Position=0)]
+    [ValidateSet('Start', 'Stop')]
+    [string]$Action,
+
+    [Parameter(Mandatory=$true, Position=1)]
+    [ValidateScript({ Test-Path $_ -PathType Leaf -ErrorAction Ignore })]
+    [string]$FilePath,
+
+    [Parameter(Mandatory=$false, Position=2)]
+    [string]$ComputerName = $env:COMPUTERNAME
+)
+
+# Obtener la lista de servicios desde el archivo especificado
+$services = Get-Content -Path $FilePath
+
+foreach ($service in $services) {
+    $status = Get-Service -Name $service -ComputerName $ComputerName -ErrorAction SilentlyContinue
+
+    if ($status) {
+        if ($status.Status -eq "Running" -and $Action -eq [Microsoft.PowerShell.Commands.ServiceControllerStatus]::Running) {
+            Write-Host "$service is already running on $ComputerName"
+        }
+        elseif ($status.Status -eq "Stopped" -and $Action -eq [Microsoft.PowerShell.Commands.ServiceControllerStatus]::Stopped) {
+            Write-Host "$service is already stopped on $ComputerName"
+        }
+        else {
+            Write-Host "$Action-ning $service on $ComputerName ..."
+
+            try {
+                if ($Action -eq [Microsoft.PowerShell.Commands.ServiceControllerStatus]::Running) {
+                    Start-Service -Name $service -ComputerName $ComputerName -ErrorAction Stop
+                }
+                else {
+                    Stop-Service -Name $service -ComputerName $ComputerName -ErrorAction Stop
+                }
+
+                Write-Host "$service has been $Action-ed on $ComputerName"
+            }
+            catch {
+                Write-Host "Error $Action-ing $service on $ComputerName: $_" -ForegroundColor Red
+            }
+        }
+    }
+    else {
+        Write-Host "Could not get status for $service on $ComputerName" -ForegroundColor Red
+    }
+}
+
+
